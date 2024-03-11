c67213a11b5573ddd2ee07b6b8102e24559ea7ed
Sayantan Paul <sayantanpaul222@gmail.com>
2024-03-12 00:51:05 +0530

    new-update

---
+++ script.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector('.menu')
+const ham_menu = document.querySelector('.ham-menu')
+const ham_menu_ul = document.querySelector('.ham-menu_ul')
+menu.addEventListener('click', openMenu)
+ham_menu.addEventListener('click', closeMenu)
+function openMenu(){
+    ham_menu_ul.classList.remove('hidden')
+}
+function closeMenu(){
+    ham_menu_ul.classList.add('hidden')
+}
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+        delay: 3000
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
